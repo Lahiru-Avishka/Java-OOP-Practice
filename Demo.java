389c5a435e2fcf65f3fd3fec5e9b669b11a1c547
Lahiru-Avishka <lahirumadushanka82887721@gmail.com>
2025-03-27 23:50:06 +0530

    list

--- Demo.java
+++ Demo.java
@@ -1,34 +1,67 @@
-class Order {
-    String id; // customer id
-    double amount;
+class List {
+    private int[] dataArray;
+    private int nextIndex;
 
-    Order(String id, double amount) {
-        this.id = id;
-        this.amount = amount;
+    public List(int intCapacity) {
+        dataArray = new int[intCapacity];
+        nextIndex = 0;
     }
+    public String toString(){
+
+    }
+
+    public int get(int index) {
+        if (index >= 0 && index < nextIndex) {
+            return dataArray[index];
+        }
+        return -1;
+    }
+
+    public void add(int data) { // add to the last
+        // if(isFull()){extendsArray();}
+        dataArray[nextIndex++] = data;
+    }
+
+    public void add(int index, int data) {
+        if (index >= 0 && index <= nextIndex) {
+            // if(isFull()){extendsArray();}
+            for (int i = nextIndex; i > index; i--) {
+                dataArray[i] = dataArray[i - 1];
+            }
+            dataArray[index] = data;
+            nextIndex++;
+        }
+    }
+
+    public void remove(int index) {
+        if (index >= 0 && index < nextIndex) {
+            for (int i = index; i < nextIndex - 1; i++) {
+                dataArray[i] = dataArray[i + 1];
+            }
+            nextIndex--;
+        }
+    }
+
+    public void printList() {
+        System.out.print("[");
+        for (int i = 0; i < nextIndex; i++) {
+            System.out.print(dataArray[i] + ", ");
+        }
+        System.out.println("\b\b]");
+    }
+    
 }
 
 class Demo {
-    public static void printOrders(Order[] orderArray) {
-       for ( Order order : orderArray ){
-            order.printOrders();
-       }
-    }
-
-    public static void sortOrderArray(Order[] orderArray) {
-    }
-
     public static void main(String args[]) {
-        Order[] orderArray = {
-                new Order("C001", 4500),
-                new Order("C002", 2500),
-                new Order("C003", 6500),
-                new Order("C004", 1500),
-                new Order("C005", 3500),
-                new Order("C005", 5500),
-        };
-        printOrders(orderArray);// [C001-4500, C002-2500, C003-6500,C004-1500,C005-3500,C005-5500]
-        sortOrderArray(orderArray);
-        printOrders(orderArray);// [C001-1500,C002-2500,C005-3500,C001-4500,C005-5500,C003-6500]
+        List list = new List(10);
+        System.out.println(list.toString()); // [empty]
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(60);
+        System.out.println(list.toString());// [10,20,30,40,50,60]
     }
 }
